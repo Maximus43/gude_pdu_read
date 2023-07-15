a0950a6e39749dc3364b2b8b0d0cb34b585dcaba
Maxim Izergin <maxim.izergin@infoss.no>
2023-07-15 23:03:17 +0200

    v.0.2, Combine all into one file for better CLI usage, remove poetry.toml

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ To use this tool, you'll need to run the main.py script with the appropriate arg
 
 Here is the general usage:
 
-```python main.py interface_name --action action_name --log_file file_path --port port_number```
+```python pdu_client.py interface_name --action action_name --log_file file_path --port port_number```
 
 - *interface_name* is the network interface to bind to.
 - *--action* is the action to perform. It can be one of the following: "log_usage_in_kw", "reset_counter", or "print_usage_in_kw". The default action is "print_usage_in_kw".
@@ -27,7 +27,7 @@ Here is the general usage:
 
 For example, to log the usage in kilowatts from the device, use the following command:
 
-```python main.py eth0 --action log_usage_in_kw --log_file ./usage_log.txt --port 23```
+```python pdu_client.py eth0 --action log_usage_in_kw --log_file ./usage_log.txt --port 23```
 
 # Using with cron for Scheduled Tasks
 
@@ -36,8 +36,8 @@ If you need to perform actions such as logging usage or resetting the counter at
 For example, you can add the following lines to your crontab to log usage at 11:59 PM every day and reset the counter at midnight on the first day of each month:
 
 ```
-59 23 * * * python /usr/share/app/pdu/main.py eth0 --action=log_usage_in_kw --log_file ./usage_log.txt
-0 0 1 * * python /usr/share/app/pdu/main.py eth0 --action=reset_counter
+59 23 * * * python /usr/share/app/pdu/pdu_client.py eth0 --action=log_usage_in_kw --log_file ./usage_log.txt
+0 0 1 * * python /usr/share/app/pdu/pdu_client.py eth0 --action=reset_counter
 ```
 
 ## License
--- pdu_client.py
+++ pdu_client.py
@@ -1,13 +1,20 @@
 """
-This module contains the PduDevice class for communicating with the PDU Gude 8311-1 device.
+A Python script to interact with the PDU Gude 8311-1 device.
 
-The PduDevice class has methods for creating a socket connection to the device and searching for devices.
+It provides options to log and print usage in kilowatts and reset the counter of the device.
+
+Usage:
+python pdu_client.py interface_name --action action_name --log_file file_path --port port_number
 """
 
+import argparse
 import socket
 import struct
+import sys
+import telnetlib
 import time
 from _socket import timeout
+from datetime import datetime
 
 
 class PduDevice(object):
@@ -102,3 +109,128 @@ class PduDevice(object):
         """
 
         self.sock.close()
+
+
+def log_usage_in_kw(host, _args):
+    """
+    Log the usage in kilowatts from the device.
+
+    Parameters:
+    host (str): The IP address of the host.
+    args (Namespace): The command-line arguments.
+
+    Returns:
+    None
+    """
+    value = execute_command(host, _args.port, command="linesensor 1 9 value show")
+    if not value:
+        value = "N/A"
+    timestamp = datetime.now().strftime("%d-%m-%Y %H:%M:%S")
+    output = f"{timestamp} - {value}"
+    with open(_args.log_file_path, "a") as file:
+        file.write(output + "\n")
+
+
+def print_usage_in_kw(host, _args):
+    """
+    Print usage in kilowatts to stdout.
+
+    Parameters:
+    host (str): The IP address of the host.
+    args (Namespace): The command-line arguments.
+
+    Returns:
+    None
+    """
+    value = execute_command(host, _args.port, command="linesensor 1 9 value show")
+    if value is not None:
+        print(value)
+
+
+def reset_counter(host, _args):
+    """
+    Reset the counter of the device.
+
+    Parameters:
+    host (str): The IP address of the host.
+    args (Namespace): The command-line arguments.
+
+    Returns:
+    None
+    """
+    execute_command(host, _args.port, command="linesensor 1 counter reset")
+
+
+def execute_command(host, port, command):
+    """
+    Executes a command on the device.
+
+    Parameters:
+    host (str): The IP address of the host.
+    port (int): The port number for the connection.
+    command (str): The command to execute.
+
+    Returns:
+    str: The output from the command execution.
+    """
+    try:
+        tn = telnetlib.Telnet(host, port)
+        tn.timeout = 1
+        tn.write(command.encode("ascii") + b"\n")
+        output = tn.read_until(b"\n").decode("ascii").replace(">", "").strip()
+        return output
+    except Exception as e:
+        print(f"Failed to execute command {command} on host {host}:{port}. Error: {str(e)}")
+        return None
+
+
+# Mapping actions to their respective function calls
+ACTION_FUNCTION_MAP = {
+    "log_usage_in_kw": log_usage_in_kw,
+    "print_usage_in_kw": print_usage_in_kw,
+    "reset_counter": reset_counter,
+}
+
+if __name__ == "__main__":
+    # Set up the argument parser
+    parser = argparse.ArgumentParser(description="Run GblClient on specific interface.")
+    parser.add_argument("interface", type=str, help="The network interface to bind to.")
+    parser.add_argument(
+        "--action",
+        type=str,
+        choices=["log_usage_in_kw", "reset_counter", "print_usage_in_kw"],
+        help="The action to perform.",
+        default="print_usage_in_kw",
+    )
+    parser.add_argument(
+        "--log_file",
+        type=str,
+        default="./stat.txt",
+        help="The file path for logging usage data.",
+    )
+    parser.add_argument(
+        "--port",
+        type=int,
+        default=23,
+        help="The port number to use for the connection.",
+    )
+
+    # Parse command-line arguments
+    args = parser.parse_args()
+
+    # Instantiate the PduDevice object and search for the device
+    pdu = PduDevice(args.interface)
+    device_list = pdu.search(max_wait=1.0, expected_devs=1)
+    if not device_list:
+        print("No devices found.")
+        sys.exit(1)
+
+    pdu_device = device_list[0]
+
+    # Perform the specified action
+    action_function = ACTION_FUNCTION_MAP.get(args.action)
+    if action_function is not None:
+        action_function(pdu_device.get("ip"), args)
+    else:
+        print(f"Invalid action: {args.action}")
+        sys.exit(1)
